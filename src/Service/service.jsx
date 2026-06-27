12118d76c7d85d4dc12cc833c9e2a4951398f5a4
surya <suryasanthoshh@gmail.com>
2026-06-27 12:26:00 +0530

    final commit

--- src/Service/service.jsx
+++ src/Service/service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useParams } from "react-router-dom";
-const base_url = "https://qsgeneraterbackend-1.onrender.com/api/question"
+const base_url = "https://qsgeneraterbackend-2.onrender.com/api/question"
 // const base_url = "http://localhost:8080/api/question"
 export default new class serive {
 
